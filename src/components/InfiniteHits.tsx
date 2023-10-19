e955878ae5ed0134d49722bab28962c924b99a2b
LukyVj <lucas.bonomi@gmail.com>
2023-10-19 16:37:06 +0200

    update

--- src/components/InfiniteHits.tsx
+++ src/components/InfiniteHits.tsx
@@ -10,26 +10,25 @@ type HitProps = {
   onHitClick: (hit: Hit) => void;
 };
 
-const CustomHit = ({
-  hit,
-  onHitClick,
-  clickedHits,
-}: HitProps & { clickedHits: string[] }) => {
-  const isClicked = clickedHits.includes(hit.objectID);
-
+const CustomHit = ({ hit, onHitClick }: HitProps) => {
   return (
     <div
-      className={`hit-clickable ${isClicked ? "clicked" : ""}`}
+      className={`ais-InfiniteHits-item__hit`}
       onClick={() => onHitClick(hit)}
     >
-      <h1>
-        <Link href={`/products/${hit.objectID}`}>
-          <Highlight attribute="name" hit={hit} />
-        </Link>
-      </h1>
-      <p>
-        <Highlight attribute="description" hit={hit} />
-      </p>
+      <Link href={`/products/${hit.objectID}`}>
+        <span className="img-container">
+          <img src={hit.image} alt={hit.name} />
+        </span>
+        <span>
+          <h1>
+            <Highlight attribute="name" hit={hit} />
+          </h1>
+          <p>
+            <Highlight attribute="description" hit={hit} />
+          </p>
+        </span>
+      </Link>
     </div>
   );
 };
@@ -42,15 +41,22 @@ export function InfiniteHits(props: InfiniteHitsProps) {
   const [selectedHit, setSelectedHit] = useState<Hit | null>(null);
   const [clickedHits, setClickedHits] = useState<string[]>([]);
 
+  // Load the clicked hits from localStorage
+  useEffect(() => {
+    const clickedHitString = localStorage.getItem("clickedHits");
+    if (clickedHitString) {
+      setClickedHits(JSON.parse(clickedHitString));
+    }
+  }, []);
+
   const openModal = (hit: Hit) => {
     setSelectedHit(hit);
     if (!clickedHits.includes(hit.objectID)) {
-      setClickedHits([hit.objectID]);
+      const updatedClickedHits = [...clickedHits, hit.objectID];
+      setClickedHits(updatedClickedHits);
+      // Save the updated clicked hits to localStorage
+      localStorage.setItem("clickedHits", JSON.stringify(updatedClickedHits));
     }
-  };
-
-  const closeModal = () => {
-    setSelectedHit(null);
   };
 
   useEffect(() => {
@@ -74,20 +80,18 @@ export function InfiniteHits(props: InfiniteHitsProps) {
   return (
     <div className="ais-InfiniteHits">
       <ul className="ais-InfiniteHits-list">
-        {hits.map((hit) => (
-          <li
-            key={hit.objectID}
-            className={`ais-InfiniteHits-item ${
-              clickedHits.includes(hit.objectID) ? "clicked" : ""
-            }`}
-          >
-            <CustomHit
-              hit={hit}
-              onHitClick={openModal}
-              clickedHits={clickedHits}
-            />
-          </li>
-        ))}
+        {hits.map((hit) => {
+          const isClicked = clickedHits.includes(hit.objectID);
+
+          return (
+            <li
+              key={hit.objectID}
+              className={`ais-InfiniteHits-item ${isClicked ? "clicked" : ""}`}
+            >
+              <CustomHit hit={hit} onHitClick={openModal} />
+            </li>
+          );
+        })}
         <li ref={sentinelRef} aria-hidden="true" />
       </ul>
     </div>
